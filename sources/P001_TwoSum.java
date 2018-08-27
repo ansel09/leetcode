8cbe6187d594e765641ddcdde4591ced99f4af1f
ansel09 <849375363@qq.com>
2018-08-27 15:39:16 +0800

    init direction 'soureces' with java solution of problem 1.

---
+++ sources/P001_TwoSum.java
@@ -0,0 +1,13 @@
+public class Solution{
+    public int[] twoSum(int[] nums, int target) { // problem 1...
+        Map<Integer,Integer> m = new HashMap<>();
+        for (int i = 0; i < nums.length ; i++) {
+            int comp = target - nums[i];
+            if(m.containsKey(comp)){
+                return new int[] {m.get(comp), i};
+            }
+            m.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No solution.");
+    }
+}
