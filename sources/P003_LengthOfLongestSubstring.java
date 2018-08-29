165f4fd2542aaa58537910ee6cb47fd647c0abfe
ansel09 <849375363@qq.com>
2018-08-29 16:20:38 +0800

    add java source code for problem 3...

---
+++ sources/P003_LengthOfLongestSubstring.java
@@ -0,0 +1,18 @@
+public class Solution{
+    public int lengthOfLongestSubstring(String s) {
+        int maxLen = 0;
+        HashMap<String, Integer> m = new HashMap();
+        for (int i = 0; i < s.length(); i++) {
+            String curr = String.valueOf(s.charAt(i));
+            Integer oldIndex = m.put(curr, i);
+            if (oldIndex != null && oldIndex != i) {
+                for (int j = i - m.size(); j < oldIndex; j++) { // 重复时，从HashMap中清除oldIndex所示位置之前的value
+                    String del = String.valueOf(s.charAt(j));
+                    m.remove(del, j); 
+                }
+            }
+            if (m.size() > maxLen) maxLen = m.size(); // 更新所保持的最大长度值
+        }
+        return maxLen;        
+    }    
+}
